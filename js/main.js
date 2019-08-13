2d0d46301e4077981d6036bd714f178665c1da4d
nicxzmiller <nicxzmiller2@gmail.com>
2019-08-13 14:18:25 +0200

    counter and blog section

--- js/main.js
+++ js/main.js
@@ -67,4 +67,15 @@ $(document).ready(function(){
         }
     });
 
+
+    $("#news-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000.2],
+        itemsMobile:[599,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay: true
+    });
 });
+
